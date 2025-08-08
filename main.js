5d7520f764343178b9833c8d558304c44afd86e6
sophi-rapozo <sophia.rapozo@escola.pr.gov.br>
2025-08-08 10:23:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
 
 const palpites = document.querySelector('.palpites');
 const ultimoResultado = document.querySelector('.ultimoResultado');
-const baixoOuAlto = document.querySelector('.baixoOuAlto');
-const envioPalpite = document.querySelector('.envioPalpite');
-const campoPalpite = document.querySelector('.campoPalpite');
-
-
-
-function verificarPalpite() {
-    const palpiteUsuario = Number(campoPalpite.value);
-    if (contagemPalpites === 1) {
-        palpites.textContent = "Palpites anteriores: ";
-    }
-
-    palpites.textContent += palpitesUsuario + " ";
-
-    if (palpiteUsuario === numeroAleatorio) {
-        ultimoResultado.textContent = "Parabens ! Você Acertou!";
-
-    }
-
-
+const baixoOuAlto = document
