753827842c1c7be34202ae2b58360959755c38f6
sophi-rapozo <sophia.rapozo@escola.pr.gov.br>
2025-08-20 07:40:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-ler numeroAleatorio = Math.floor(Math.random() *100) + 1;
+let numeroAleatorio = Math.floor(Math.random() *100) + 1;
 const palpites = document.querySelector('.palpites');
 const ultimoResultado = document.qurerySelector('.ultimoResultado');
 const baixoOuAlto = document.querySelector('.baixoOuAlto');
